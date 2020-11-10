2c7b5150cf2aae8870caecf4b5b4f28d4d2ff7c1
Diego Mendes <gdmendes55@gmail.com>
2020-11-10 15:59:23 -0400

    next js

--- pages/home.js
+++ pages/home.js
@@ -1,30 +1,12 @@
-import Head from 'next/head';
-import { Navbar, NavDropdown } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Link } from 'react-bootstrap/lib/Navbar';
+import './_app';
+function Home() {
 
-// import { Navbar, Nav, NavDropdown } from 'react-bootstra';
+    return (
 
-const Home = () => (
-    <div style={{
-        marginBottom: '5%'
-    }}>
-        <Head>
-            <title>App Next.js</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand href="/">SmashLog tecnologia</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav>
-                    <NavDropdown title="News" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="/business">Negocios</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="/technology">Tecnologia</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    </div>
-);
+        <div>Home</div>
+    );
+};
 
 export default Home;
