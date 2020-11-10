69ea4c9f29b132253d77acd72e5fc4e416135c04
Diego Mendes <gdmendes55@gmail.com>
2020-11-10 16:16:26 -0400

    next link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-bootstrap';
+import { Link } from 'next/link';
 import './_app';
 function index() {
   const [campos, setCampos] = useState({
